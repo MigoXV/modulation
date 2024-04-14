7ba20903d794fc545af9a077aceb44d8ce525e46
migo <1695587905@qq.com>
2024-04-15 00:35:59 +0800

    验证集前向传播时输出nan，待解决

--- trainer/validator.py
+++ trainer/validator.py
@@ -3,6 +3,7 @@ import wandb
 
 from sklearn.metrics import precision_score, recall_score, f1_score
 
+from textbrewer.distiller_utils import move_to_device
 
 class Validator:
     
@@ -15,7 +16,7 @@ class Validator:
         
     def callback(self, model, step):
         metrics = validate(model, self.dataloader, self.loss_fn, self.device)
-        log_data = metrics.copy
+        log_data = metrics.copy()
         self.epoch += 1
         log_data["step"] = step
         log_data["epoch"] = self.epoch
@@ -33,9 +34,10 @@ def validate(model, dataloader, loss_fn, device):
 
     with torch.no_grad():
         for batch in dataloader:
+            batch = move_to_device(batch, device)
             inputs, labels = batch
-            inputs, labels = inputs.to(device), labels.to(device)
-            outputs = model(inputs)
+            # inputs, labels = inputs.to(device), labels.to(device)
+            outputs = validate_on_batch(device,model,batch, {})
             loss = loss_fn(outputs, labels)
             total_loss += loss.item()
             _, predicted = torch.max(outputs, 1)
@@ -57,3 +59,12 @@ def validate(model, dataloader, loss_fn, device):
         "recall": recall,
         "f1": f1,
     }
+    
+def validate_on_batch(device,model,batch, args) -> torch.Tensor:
+    batch = move_to_device(batch, device)
+    if type(batch) is dict:
+        results = model(**batch,**args)
+    else:
+        results = model(*batch, **args)
+
+    return results
